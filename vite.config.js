39f86dc30440eacf77c207f5e98c9a9bb891c360
Muhammad Fahmi Izzuddin Bin Mohd Idrus <fahmiizzuddin182@gmail.com>
2025-12-27 17:03:12 +0800

    Prepare for GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/paws-and-preferences/",
   plugins: [react(), tailwindcss()],
 });
